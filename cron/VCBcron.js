71abc74dbd80df4962f4160b891e0dba4bc976b1
Lam Ho <hophamlam92@yahoo.com>
2022-08-18 16:21:11 +0700

    go

--- cron/VCBcron.js
+++ cron/VCBcron.js
@@ -17,8 +17,35 @@ var vcbFetchLink = "http://103.130.219.9:4000/api/vcb/transactions"
 // console.log(vcbFetchLink)
 console.log(new Date().toString())
 
+const sendMessageDiscord = () => {
+  var messageDiscord = "Vietcombank lỗi " + new Date();
+  var axios = require('axios');
+  var FormData = require('form-data');
+  var data = new FormData();
+  data.append('content', messageDiscord);
+  
+  var config = {
+    method: 'post',
+    url: 'https://discord.com/api/webhooks/1008656290463109201/6YCO-J9pIF0EZv_4cylRueuPdvXU4KR2R9u3Rt8QmRUZrRtKKKj32qW0Fi1WAuA6DFRH',
+    headers: { 
+      'Content-Type': 'application/x-www-form-urlencoded', 
+      'Cookie': '__cfruid=be29ca0b0acd77d393277ec967425509a92b7aba-1660553750; __dcfduid=0f7577621c7811edbf7cdeffafb5e155; __sdcfduid=0f7577621c7811edbf7cdeffafb5e155fa163fdf4b26c835f5b8fa76777cef0c6ea7b05755fff7e0a324e03446dc7f86', 
+      ...data.getHeaders()
+    },
+    data : data
+  };
+  
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  
+}
+
 //test if works fine -> log "ko giao dịch" ; if api backend fail -> send discord message
-sendMessageDiscord()
 
 cron.schedule('*/3 * * * *', () => {
 //Run every 4 minutes, between 06:00 AM and 07:59 PM
@@ -67,35 +94,10 @@ const checkVCB = () => {
     console.log(error);
   });
 }
-const sendMessageDiscord = () => {
-  var messageDiscord = "Vietcombank lỗi " + new Date();
-  var axios = require('axios');
-  var FormData = require('form-data');
-  var data = new FormData();
-  data.append('content', messageDiscord);
-  
-  var config = {
-    method: 'post',
-    url: 'https://discord.com/api/webhooks/1008656290463109201/6YCO-J9pIF0EZv_4cylRueuPdvXU4KR2R9u3Rt8QmRUZrRtKKKj32qW0Fi1WAuA6DFRH',
-    headers: { 
-      'Content-Type': 'application/x-www-form-urlencoded', 
-      'Cookie': '__cfruid=be29ca0b0acd77d393277ec967425509a92b7aba-1660553750; __dcfduid=0f7577621c7811edbf7cdeffafb5e155; __sdcfduid=0f7577621c7811edbf7cdeffafb5e155fa163fdf4b26c835f5b8fa76777cef0c6ea7b05755fff7e0a324e03446dc7f86', 
-      ...data.getHeaders()
-    },
-    data : data
-  };
-  
-  axios(config)
-  .then(function (response) {
-    console.log(JSON.stringify(response.data));
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-  
-}
+
   // checkVCB()
   console.log(new Date().toString())
+  sendMessageDiscord()
 })
 
 }
